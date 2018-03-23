53dfc7458ff6a25ebb2bcb19d81717ef0e3461bb
Artyom Lisovskij <artyom@lisovskij.ru>
2018-03-23 21:37:25 +0400

    fix

--- bot.py
+++ bot.py
@@ -10,12 +10,15 @@ from time import sleep
 
 API_TOKEN = '522766611:AAE0Yv-fxf35pAk_bU4MIzZtfPPym4Z4DpM'
 
-WEBHOOK_HOST = 'dynamic-door.ru'
+WEBHOOK_HOST = '85.143.209.253'
 WEBHOOK_PORT = 8443  # 443, 80, 88 or 8443 (port need to be 'open')
 WEBHOOK_LISTEN = '0.0.0.0'  # In some VPS you may need to put here the IP addr
 
-WEBHOOK_SSL_CERT = '/etc/dehydrated/certs/dynamic-door.ru/fullchain.pem'  # Path to the ssl certificate
+WEBHOOK_SSL_CERT = './webhook_cert.pem'  # Path to the ssl certificate
 WEBHOOK_SSL_PRIV = '/etc/dehydrated/certs/dynamic-door.ru/privkey.pem'  # Path to the ssl private key
+
+WEBHOOK_SSL_CERT = './webhook_cert.pem'  # Path to the ssl certificate
+WEBHOOK_SSL_PRIV = './webhook_pkey.pem'  # Path to the ssl private key
 
 # Quick'n'dirty SSL certificate generation:
 #
