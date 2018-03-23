e368bd8a42a4a0e971b431f1540757024643853b
dip96 <dipp962@gmail.com>
2018-03-24 03:46:21 +0400

    fix

--- bot.py
+++ bot.py
@@ -176,7 +176,7 @@ def process_choose(message):
         keyboard = types.ReplyKeyboardMarkup(one_time_keyboard=True,selective=True)
         print(all_items[0].name)
         for i in range(len(all_items)):
-            keyboard.row(types.KeyboardButton(all_items[i].names))
+            keyboard.row(types.KeyboardButton(all_items[i].name))
     else:
         bot.reply_to(message, "Команда не распознана")
         bot.send_message(chat_id, "Выберите нужный пункт меню", reply_markup=menu(message))
