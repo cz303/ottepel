a885e0dfc0040f442df77157cf184401a765cebd
dip96 <dipp962@gmail.com>
2018-03-24 19:16:24 +0400

    fi

--- bot.py
+++ bot.py
@@ -225,7 +225,7 @@ def process_choose(message):
         keyboard.add(*[types.KeyboardButton('Создать категорию')])
         for n in chat_category:
             keyboard.add(*[types.KeyboardButton('Категория: ' + n)])
-        bot.send_message(message.chat.id, 'Выберите нужную категорию, если её нет, то создайте', reply_markup=keyboard)
+        bot.send_message(message.chat.id, 'Выберите нужную категорию, если её нет, то создайте')
             # bot.send_message(message.chat.id, n)
         bot.register_next_step_handler(message, new_category)
     elif message.text == 'Получить информацию о магазине':
@@ -270,9 +270,10 @@ def new_category(message):
     chat_id = message.chat.id
     one_item = Ecommerce.query.filter_by(chat_id=chat_id).first()
     one_item.category = message.text
-    if one_item.category:
-        bot.send_message(chat_id, "Такая категория уже существует")
-        bot.register_next_step_handler(message, new_category)
+    
+    # if one_item.category:
+    #     bot.send_message(chat_id, "Такая категория уже существует")
+    #     bot.register_next_step_handler(message, new_category)
 
 def new_items(message):
     chat_id = message.chat.id
