027abe95d0028a6604ca265d9955c390d7a41ed1
Artyom Lisovskij <artyom@lisovskij.ru>
2018-03-24 14:30:07 +0400

    fix

--- bot.py
+++ bot.py
@@ -26,7 +26,7 @@ WEBHOOK_HOST = 'dynamic-door.ru'
 # When asked for "Common Name (e.g. server FQDN or YOUR name)" you should reply
 # with the same value in you put in WEBHOOK_HOST
 
-WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
+WEBHOOK_URL_BASE = "https://%s" % (WEBHOOK_HOST)
 WEBHOOK_URL_PATH = "/%s/" % (API_TOKEN)
 
 
