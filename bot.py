ac6612d8695445513882b8d1380733d3698631a6
Artyom Lisovskij <artyom@lisovskij.ru>
2018-03-24 10:30:02 +0400

    fix

--- bot.py
+++ bot.py
@@ -251,14 +251,18 @@ def new_slug(message):
 def new_picture(message):
     chat_id = message.chat.id
     new_item = Item.query.filter_by(filled=False, market_id=chat_id).first()
-    print(message.photo)
-    file_info = bot.get_file(message.photo.file_id)
-    downloaded_file = 'https://api.telegram.org/file/bot' + API_TOKEN + '/' + file_info.file_path
-    print(downloaded_file)
-    new_item.picture = downloaded_file
-    new_item.filled = True
-    db.session.commit()
-    bot.send_message(chat_id, "Выберите дальнейшее действие", reply_markup=menu(message))
+    print(message.photo[0])
+    if message.photo:
+        file_info = bot.get_file(message.photo[0].file_id)
+        downloaded_file = 'https://api.telegram.org/file/bot' + API_TOKEN + '/' + file_info.file_path
+        print(downloaded_file)
+        new_item.picture = downloaded_file
+        new_item.filled = True
+        db.session.commit()
+        bot.send_message(chat_id, "Выберите дальнейшее действие", reply_markup=menu(message))
+    else:
+        bot.send_message(chat_id, "Это была не картинка. Нужна Картинка!")
+        bot.register_next_step_handler(message, new_picture)
     
 def new_location(message):
     chat_id = message.chat.id
