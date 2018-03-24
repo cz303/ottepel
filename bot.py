382fa6f8d5ba826f82ef2945c700f512665a8645
dip96 <dipp962@gmail.com>
2018-03-24 15:21:35 +0400

    fix

--- bot.py
+++ bot.py
@@ -68,14 +68,16 @@ class Ecommerce(db.Model):
 class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255))
+    category = db.Column(db.String(255))
     price = db.Column(db.Integer)
     picture = db.Column(db.PickleType())
     market_id = db.Column(db.Integer)
     category_id = db.Column(db.Integer)
     filled = db.Column(db.Boolean, default=False, nullable=False)
 
-    def __init__(self, name='', price=0, picture=None, market_id=0, filled=False, category_id=0):
+    def __init__(self, name='', category='', price=0, picture=None, market_id=0, filled=False, category_id=0):
         self.name = name
+        self.category = category
         self.price = price
         self.picture = picture
         self.market_id = market_id
