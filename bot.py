fa68b9d653b371b3ebd80781fb62b60d8e78869f
dip96 <dipp962@gmail.com>
2018-03-24 19:31:15 +0400

    fix

--- bot.py
+++ bot.py
@@ -275,8 +275,7 @@ def new_category(message):
             if message.text == n:
                 bot.send_message(chat_id, "Данная категория существует, посмотрите внимательней")
             else:
-            	bot.send_message(chat_id, "Данная категория создана"!)
-                chat_category.append(message.text)
+            	bot.send_message(chat_id, "Данная категория создана!")
     # if one_item.category:
     #     bot.send_message(chat_id, "Такая категория уже существует")
     #     bot.register_next_step_handler(message, new_category)
