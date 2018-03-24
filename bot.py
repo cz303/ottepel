150a24c68e31b34dfb4455368abdd1440e674118
dip96 <dipp962@gmail.com>
2018-03-24 16:13:45 +0400

    fix

--- bot.py
+++ bot.py
@@ -226,9 +226,11 @@ def new_market(message):
 
 def new_category(message):
     chat_id = message.chat.id
-    one_item.category = message.text
+    one_item = Ecommerce.query.filter_by(chat_id=chat_id).first()
+    one_item.category_id = message.text
+    # db.session.add(new_item)
     db.session.commit()
-    print(one_item.category)
+    print(one_item.category_id)
     bot.send_message(chat_id, "Введитие название товара")
     bot.register_next_step_handler(message, new_items)
 
