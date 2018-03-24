36cd314ab4605c4b0431a106cb6e385b671282c5
dip96 <dipp962@gmail.com>
2018-03-24 17:00:20 +0400

    fix

--- bot.py
+++ bot.py
@@ -347,7 +347,7 @@ def items_slider(chat_id, list_items, item_id):
     if next_id > len(list_items) - 1:
         next_id = 0
 
-    row.append(types.InlineKeyboardButton("ID " + str(list_items[item_id].id) + " "+ list_items[item_id].name + " " + chat_category[chat_id], callback_data="ignore"))
+    row.append(types.InlineKeyboardButton("ID " + str(list_items[item_id].id) + " "+ list_items[item_id].name + " " + chat_category[list_items[item_id].id], callback_data="ignore"))
 
     markup.row(*row)
     row=[]
