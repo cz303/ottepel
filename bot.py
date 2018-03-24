43db6ace390a92ece5a97ef80f7b1c347592bb22
dip96 <dipp962@gmail.com>
2018-03-24 18:57:05 +0400

    fix

--- bot.py
+++ bot.py
@@ -221,8 +221,10 @@ def process_choose(message):
         bot.send_message(chat_id, "Введите название магазина")
         bot.register_next_step_handler(message, new_market)
     elif message.text == 'Добавить товар':
+    	keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
         for n in chat_category:
-            bot.send_message(message.chat.id, n)
+            keyboard.add(*[types.KeyboardButton('Категория: ' + n)])
+            # bot.send_message(message.chat.id, n)
         bot.send_message(chat_id, "Введитие категорию товара")
         bot.register_next_step_handler(message, new_category)
     elif message.text == 'Получить информацию о магазине':
