aecd4d957991ae151fffb6330fe4767edaa0ecc7
dip96 <dipp962@gmail.com>
2018-03-24 09:37:31 +0400

    fix

--- bot.py
+++ bot.py
@@ -306,10 +306,10 @@ def previous_item(call):
     bot.edit_message_text("Товары", call.from_user.id, call.message.message_id, reply_markup=markup)
     bot.answer_callback_query(call.id, text="")
 
-@bot.callback_query_handler(func=lambda call: call.data == 'to_menu')
+@bot.callback_query_handler(func=lambda call: call.data[0:9] == 'to_menu')
 def to_menu(call):
 	chat_id = call.message.chat.id
-    bot.send_message(chat_id, "Выберите дальнейшее действие", reply_markup=menu(message))
+    bot.send_message(chat_id, "Выберите дальнейшее действие", reply_markup=menu(call))
 
 # Remove webhook, it fails sometimes the set if there is a previous webhook
 bot.remove_webhook()
