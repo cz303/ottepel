c226bdb04392498eaf633c7308076de14a0608e1
Anton Kuzmin <33873098+CauseMean@users.noreply.github.com>
2018-03-25 11:31:51 +0400

    f

--- bot.py
+++ bot.py
@@ -718,7 +718,9 @@ def delete_item(message):
     chat_id = message.chat.id
     item_num = chat_dict[chat_id] 
     if message.text == 'Да':
-        Orders.query.filter_by(item_id=chat_id).delete() 
+        one_item = Orders.query.filter_by(item_id=chat_id).one()
+        db.session.delete(one_item)
+        db.session.commit() 
         bot.send_message(chat_id, "Удалено!")
         bot.send_message(chat_id, "Выберите нужный пункт редактирования", reply_markup=edit_menu(message, item_num))
     else:
